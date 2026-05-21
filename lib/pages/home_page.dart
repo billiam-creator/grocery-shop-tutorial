91a422ce8d4e8ee7c2ff399272c7c1c989d888bf
unknown <bushg5200@gmail.com>
2026-05-21 11:53:47 +0300

    add floating action button navigating to cartPage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:grocery_app/components/grocery_item_tile.dart';
 import 'package:grocery_app/models/cart_model.dart';
+import 'package:grocery_app/pages/cart_page.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,6 +11,14 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: 
+      () => Navigator.push(context, MaterialPageRoute(builder: (context){
+        return CartPage();
+      }
+      )),
+      backgroundColor: Colors.black,
+      child: Icon(Icons.shopping_cart,
+      color: Colors.white,),),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
